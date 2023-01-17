ac057c9746819729b5a6214b4814ad96a9375e49
gabigab117 <gabrieltrouve5@outlook.com>
2023-01-17 21:33:57 +0100

    Update models.py
    add class

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 
 # Create your models here.
@@ -6,10 +7,23 @@ class BlogPost(models.Model):
     # pour CharField il faut absolument définir une longueur en param
     title = models.CharField(max_length=100)
     # pour transformer title en url on utilise un slug
-    slug = models.SlugField()
+    slug = models.SlugField(blank=True)
     # valeur par défaut en param
     published = models.BooleanField(default=False)
     # blank permet de spécifier que je peux laisser vide. Sinon obligé de renseigner date
     date = models.DateField(blank=True, null=True)
     content = models.TextField()
     description = models.TextField()
+
+    # une info que je ne veux pas forcémment stocker dans ma BDD mais juste afficher ==>
+    def publish_string(self):
+        if self.published:
+            return "L'article est publié"
+        return "L'article est inaccessible"
+
+    def save(self, *args, **kwargs):
+
+        if not self.slug:
+            self.slug = slugify(self.title)
+
+        super().save(*args, **kwargs)
