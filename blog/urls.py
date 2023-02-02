8a1da9f8634e60866422eb92bcb11f186b672c47
gabigab117 <gabrieltrouve5@outlook.com>
2023-02-02 23:20:06 +0100

    CRUD

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path
-from blog.views import blog_post, blog_posts, echappement, etendre, etendu, etendupost, blog_post_form
+from blog.views import blog_post, blog_posts, echappement, etendre, etendu, etendupost, blog_post_form, BlogIndexView,\
+    BlogPostDetailView
 
 urlpatterns = [
-    path('', blog_posts, name="blog-index"),
+    # path('', blog_posts, name="blog-index"),
+    # url pour la vue ListViw qui remplace blog_posts ci-dessus, penser au as_view() pour créer l'instance
+    path('', BlogIndexView.as_view(), name="blog-index"),
     path('echap/', echappement, name="echap"),
     path('etendre/', etendre, name="etendre-template"),
     path('etendu/', etendu, name="template-etendu"),
     path('post-pk<str:pk>/', etendupost, name="etendu-post"),
-    path('post-<str:slug>/', blog_post, name="blog-post"),
+    # path('post-<str:slug>/', blog_post, name="blog-post"),
+    # url pour DetailView qui remplace blog_post
+    path('post-<str:slug>/', BlogPostDetailView.as_view(), name="blog-post"),
 
     # ci-dessous urls pour les forms
     path('article/', blog_post_form, name="blog-post-form")
--- blog/views.py
+++ blog/views.py
@@ -3,11 +3,35 @@ from blog.models import BlogPost
 from blog.forms import BlogPostForm
 from datetime import datetime
 from django.http import HttpResponseRedirect
+from django.views.generic import ListView, DetailView
 
 
 def blog_posts(request):
     posts = BlogPost.objects.all()
+    # ou
+    posts2 = BlogPost.objects.filter(published=True)
     return render(request, "blog/index.html", context={"posts": posts})
+
+
+# a la place de la vue basée sur la fonction blog_posts on va créer une class ListView
+class BlogIndexView(ListView):
+    # ci-dessous c'est comme si je faisais model = BlogPost.objects.all()
+    # ca retourne tous nos objets dans notre context ds une variable object_list
+    model = BlogPost
+    # mais si je veux récupérer certains articles on ne fait pas model = BlogPost :
+    # queryset = BlogPost.objects.filter(published=True)
+    template_name = "blog/index_listview.html"
+    # pour remplacer le nom de la variable object_list :
+    context_object_name = "articles"
+
+
+# on va récupérer un article
+class BlogPostDetailView(DetailView):
+    # ca retourne notre objet dans notre context ds une variable object
+    model = BlogPost
+    template_name = "blog/post_detailview.html"
+    context_object_name = "post"
+    # même pas besoin de passer en argument slug, on le passe dans l'url et django retrouve notre champs slug
 
 
 def blog_post(request, slug):
@@ -48,7 +72,7 @@ def blog_post_form(request):
         if form.is_valid():
             # je sauvegarde le formulaire avec form.save()
             # le paramètre commit=True par def. Donc si pas de param l'objet est sauvegardé dans la BDD
-            # Avec commit=False je peux récuprer l'objet dans une variable pour le modifier
+            # Avec commit=False je peux récupérer l'objet dans une variable pour le modifier
             # blog_post = form.save(commit=False)
             # blog_post.published = True
             # blog_post.save()
